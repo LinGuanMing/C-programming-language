7aa80a7047495b8473d402155120e104465584f6
solitaryseal <solitaryseal@gmail.com>
2017-09-20 01:44:36 -0400

    Operating scanf

---
+++ scanf/scanf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+	char str1[20],str2[20];
+printf("Enter value1:");
+scanf("%s", str1);
+
+printf("Enter value2:");
+scanf("%s", str2);
+
+printf("Entered value1=%s value2=%s\n", str1, str2);
+return 0;
+}
